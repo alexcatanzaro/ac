3ee76707f7406c716a56ae5ff31ccb9bedfc19c1
alexcatanzaro <alexcatanzaro@gmail.com>
2019-07-03 22:37:46 -0500

    cleaning things up a bit

--- SiteFiles/src/main.js
+++
@@ -1,9 +0,0 @@
-
-let start = '{"favColor":{"first":"green","second":"blue"},"favState":"Texas"}';
-
-let myObject = JSON.parse(start);
-
-console.log(myObject);
-
-document.getElementById('display').innerHTML = myObject.favState;
-document.getElementById('display2').innerHTML = myObject.favColor.first;
